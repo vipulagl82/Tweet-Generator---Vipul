92dad7757eb580fe19930497417a120534dbb6c4
vipulagl82 <iitkvipul@gmail.com>
2025-05-16 11:46:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from langchain import PromptTemplate
 import streamlit as st
 import os
 
-os.environ['GOOGLE_API_KEY'] = st.secrets['AIzaSyBKueY9jDDeSymH8jRwLkGT9-Tks5ZEoVw']
+os.environ['GOOGLE_API_KEY'] = st.secrets['GOOGLE_API_KEY']
 
 # Create prompt template for generating tweets
 
